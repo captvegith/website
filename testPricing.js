eee228235585454bad208be561382fbefdfa96eb
captvegith <95250189+captvegith@users.noreply.github.com>
2022-01-24 15:09:42 +0100

    Update testPricing.js

--- testPricing.js
+++ testPricing.js
@@ -102,33 +102,34 @@
             }
         }    
     });
-    function numberOfPlantes(){
-        var qtyOfPlantes = 0; 
-        var theForm = document.forms["pricingPlantes"];
-        var plantesQuantity = theForm.elements["quantity"];
-
-        for (var i = 0; i < plantesQuantity.length; i++) {
-
-                qtyOfPlantes += parseInt(plantesQuantity[i].value);
-        }
-        return qtyOfPlantes; 
-    }; 
-    document.getElementById('totalPlanteQty').value = numberOfPlantes(); 
-
-    // Cout entretien par mois 
-    function costEntretien() {
-        var nombreOfPlantes = numberOfPlantes(); 
-        var coutEntretien = 0; 
-
-        if(document.getElementById('offerAchatEntretien').checked || (document.getElementById('offerLocation').checked)) {
-            coutEntretien = nombreOfPlantes + 60; 
-        }; 
-        return coutEntretien; 
-    }; 
-    document.getElementById('coutEntretienT').value = costEntretien(); 
 
 
-       // cout rempotage par mois 
+    // + - des inputs quantités de plantes 
+    function wcqib_refresh_quantity_increments() {
+        jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
+            var c = jQuery(b);
+            c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+        })
+    }
+    String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+        var a = this,
+            b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+        return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+    }), jQuery(document).ready(function() {
+        wcqib_refresh_quantity_increments()
+    }), jQuery(document).on("updated_wc_div", function() {
+        wcqib_refresh_quantity_increments()
+    }), jQuery(document).on("click", ".plus, .minus", function() {
+        var a = jQuery(this).closest(".quantity").find(".qty"),
+            b = parseFloat(a.val()),
+            c = parseFloat(a.attr("max")),
+            d = parseFloat(a.attr("min")),
+            e = a.attr("step");
+        b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+    });
+
+
+// cout rempotage par mois 
        function costRempotage() {
         var nombreOfPlantes = numberOfPlantes(); 
         var coutRempotage = 0; 
@@ -140,18 +141,50 @@
     document.getElementById('coutRempotage').value = costRempotage(); 
 });
 
-// fonction calcul de la quantité totale de plantes 
-    // // fonction cout plantes avec pots : 
-    // function plantesPlusPots(){
-    //     var 
-    // }
+/// GOOOOOD ---> Nombre de plantes détermine Cout entretien / Cout Location / Cout rempotage 
+function numberOfPlantes(){
+    var arrayOfQuantity = document.getElementsByName('quantity');
+    var qtyOfPlantes = 0; 
 
-    function costLocation() {
-        
+    for (var i = 0; i < arrayOfQuantity.length; i++) {
+            if(parseInt(arrayOfQuantity[i].value))
+            qtyOfPlantes += parseInt(arrayOfQuantity[i].value);
     }
+    document.getElementById('totalPlanteQty').value = qtyOfPlantes; 
+
+    // ------------ valeur de base de calcul ------------------- 
+    var nombreOfPlantes = document.getElementById('totalPlanteQty').value; 
+
+    // -------------  calcul cout entretien --------------------- 
+    var coutEntretien = 0; 
+    if(document.getElementById('offerAchatEntretien').checked || (document.getElementById('offerLocation').checked)) {
+        coutEntretien = parseInt(nombreOfPlantes) + 60; 
+    }; 
+    document.getElementById('coutEntretienT').value = coutEntretien; 
+
+    // ------------- calcul cout location ------------------------ 
+    var coutLocation = 0; 
+    if(document.getElementById('offerLocation').checked) {
+        coutLocation = parseInt(nombreOfPlantes) + 60; 
+    }
+    document.getElementById('coutLocationT').value = coutLocation; 
+
+    // ------------- calcul cout rempotage ------------------------ 
+    var coutRempotage = 0; 
+    if(document.getElementById('offerAchatEntretien').checked || (document.getElementById('offerLocation').checked)) {
+        coutRempotage = parseInt(nombreOfPlantes);
+    }
+    document.getElementById('coutRempotageT').value = coutRempotage;
 
 
-    // Calcul CoutTotal
+
+    // ----------- calcul cout plantes avec pots ------------------
+    var coutPlantesPots = 0; 
+
+}; 
+
+
+// Calcul CoutTotal
     function plantesPriceT() {
        
 
@@ -210,9 +243,7 @@
             coutLocationT.value = coutLocation; 
         }
 
-       
-
-
+    
         // Calcul cout total TOTAL 
         var livraisonInstallation = 89; 
         if (document.getElementById('offerLocation').checked) {
@@ -242,62 +273,4 @@
         // pricePlanteT.value = pricePlanteTotal;
     };
 
-    function wcqib_refresh_quantity_increments() {
-        jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
-            var c = jQuery(b);
-            c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
-        })
-    }
-    String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-        var a = this,
-            b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-        return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-    }), jQuery(document).ready(function() {
-        wcqib_refresh_quantity_increments()
-    }), jQuery(document).on("updated_wc_div", function() {
-        wcqib_refresh_quantity_increments()
-    }), jQuery(document).on("click", ".plus, .minus", function() {
-        var a = jQuery(this).closest(".quantity").find(".qty"),
-            b = parseFloat(a.val()),
-            c = parseFloat(a.attr("max")),
-            d = parseFloat(a.attr("min")),
-            e = a.attr("step");
-        b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
-    });
 
-    // Calcul Plantes Pots HT  function getPlantesPrice () ? 
-
-
-    // Calcul Cout Entretien? function getEntretienPrice() ? 
-
-    // Calcul cout Location function getLocationPrice () ? 
-
-    // Calcul du total function getTotal()? 
-
-// // Calcul CoutTotal
-// function plantesPriceT() {
-
-//     var prixPlantesPots = document.getElementById('prixPlantesPots');
-
-//     // babyPlante
-//     babyPlantePots = document.getElementById('babyPlantePots').value; 
-//     babyPlanteQty = document.getElementById('babyPlanteQty').value; 
-//     // babyPlanteT = parseInt(babyPlantePots) * parseInt(babyPlanteQty);
-//     babyPlanteT = parseInt(babyPlantePots) * parseInt(babyPlanteQty);
-
-//     // teenagePlante
-//     teenagePlantePots = document.getElementById('teenagePlantePots').value; 
-//     teenagePlanteQty = document.getElementById('teenagePlanteQty').value; 
-//     // teenagePlanteT = document.getElementById("teenagePlanteT").value = teenagePlanteU * teenagePlanteQty;
-//     teenagePlanteT = parseInt(teenagePlantePots) * parseInt(teenagePlanteQty);
-
-//     // tombantePlante
-
-
-// function coutPlantesPot(){} => 
-// function coutRempotage(){
-    // var coutRempotage = 0; 
-    // récupération du nombre de plantes comme le cout d'entretien 
- //}
- 
-// function prixTotal = 
